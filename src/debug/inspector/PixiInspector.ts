0f60fac8eafd1e74e6df9d408cb9c159eb9f60dc
Pi-Bouf <pierre.bouffier05@gmail.com>
2022-01-10 22:42:10 +0100

    Add list & buttons to the inspector

--- src/debug/inspector/PixiInspector.ts
+++ src/debug/inspector/PixiInspector.ts
@@ -9,16 +9,17 @@ export class PixiInspector {
         this.container = container;
 
         this.mainBoxStyle = `
-            background-color: rgba(0, 0, 0, 0.7); 
+            background-color: rgba(0, 0, 0, 0.5); 
             color: white; 
             font-family: "Tahoma", sans-serif;
             font-size: 14px; 
-            width: 300px; 
-            height: 400px; 
+            width: 400px;
+            padding: 6px;
+            border-radius: 5px;
             position: absolute; 
             z-index: 9999999; 
-            top: 0; 
-            left: 0;` + styleOverride;
+            top: 5; 
+            left: 5;` + styleOverride;
 
         this.buildInterface();
     }
@@ -27,7 +28,18 @@ export class PixiInspector {
         const mainBox = document.createElement('div');
         mainBox.setAttribute('style', this.mainBoxStyle);
 
+        const mainList = document.createElement('div');
+        mainList.setAttribute('style', 'height: 500px; background-color: rgba(0, 0, 0, 0.3); border-radius: 5px;');
 
+        const mainButtonBox = document.createElement('div');
+        mainButtonBox.setAttribute('style', 'margin-top: 5px;');
+
+        const refreshButton = document.createElement('button');
+        refreshButton.innerText = 'Refresh';
+
+        mainBox.appendChild(mainList);
+        mainButtonBox.appendChild(refreshButton);
+        mainBox.appendChild(mainButtonBox);
 
         document.body.appendChild(mainBox);
     }
